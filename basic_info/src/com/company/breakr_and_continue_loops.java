20534047045938d033d19aa144c592da09be9677
hammad khan <58707477+Hammadkhan12@users.noreply.github.com>
2022-02-04 05:35:51 -0500

    Update breakr_and_continue_loops.java

--- basic_info/src/com/company/breakr_and_continue_loops.java
+++ basic_info/src/com/company/breakr_and_continue_loops.java
@@ -3,6 +3,10 @@ package com.company;
 public class breakr_and_continue_loops {
     public static void main(String[] args) {
 //        Break and continue using loops!
+//         loops main breakr or continue ka basic kaam bhaot he simple hai
+//         breake kise bhee loop ke iterration ko end krd deta hai vaheen jahan usko(loop) breake mila;
+//         or continue sirf us iterration ko skip krta hai jahan usko(loop) continue mila;
+//         
         for (int i=0;i<50;i++){
             System.out.println(i);
             System.out.println("Java is great");
