3d7189afd6fd901e8094aff8c88d6926687a1908
hammad khan <58707477+Hammadkhan12@users.noreply.github.com>
2022-02-07 13:03:28 -0500

    Add files via upload

---
+++ basic_info/src/com/company/methods_overloading.java
@@ -0,0 +1,65 @@
+package com.company;
+
+public class methods_overloading {
+    public static void main(String[] args) {
+
+//    In Java, it is possible for a class
+//    to contain two or more methods with the same name but with different parameters.
+//    Such methods are called Overloaded methods.
+//    Method overloading is used to increase the readability of the program.
+
+
+//---------------exaple----------------------------
+
+//void foo()
+//    void foo(int a)	//Overloaded function foo
+//    int foo(int a, int b)
+
+//    By changing the return type of the different methods
+//    By changing the number of arguments accepted by the method
+//class calculate{
+//    int multiply(int a,int b){
+//        return a*b;
+//    }
+//    double multiply(double a,double b){
+//        return  a*b;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        calculate obj = new calculate();
+//        int c = obj.multiply(5,4);
+//        double d = obj.multiply(5.1,4.2);
+//        System.out.println("Mutiply method : returns integer : " + c);
+//        System.out.println("Mutiply method : returns double : " +  d);
+//    }
+
+//        -----------------2nd example------------
+//        again we created an other methord but this time the ayrguments are 3
+//        class calculate{
+//            int multiply(int a,int b){
+//                return a*b;
+//            }
+//            int multiply(int a,int b,int c){
+//                return  a*b*c;
+//            }
+//
+//            public static void main(String[] args) {
+//
+//                calculate obj = new calculate();
+//                int c = obj.multiply(5,4);
+//                int d = obj.multiply(5,4,3);
+//                System.out.println(c);
+//                System.out.println(d);
+//
+//            }
+//        }
+
+//        these two methods are overloaded because they have the same name but their return is different.
+
+
+    }
+}
+
+
+
